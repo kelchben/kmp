af4b66d2e6d60879b91cd26eda5c512a72556fa7
kelchben <Benjamin.Kelch@student.hu-berlin.de>
2018-01-10 14:51:55 +0100

    intellij

--- src/hsms.java
+++ src/hsms.java
@@ -2,4 +2,11 @@
  * Created by kcots on 10.01.2018.
  */
 public class hsms {
+
+        public static void main(String[] args) {
+    Main dings = new Main();
+    String[] hi = new String[1];
+    dings.main(hi);
+            System.out.println("Hello World!1");
+        }
 }
