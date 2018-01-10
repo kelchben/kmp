ca6b27ecde5c673fffba9ec3bbca49048dc1b6f0
kelchben <Benjamin.Kelch@student.hu-berlin.de>
2018-01-10 15:47:38 +0100

    intellij

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        basicsearch suche = new basicsearch();
+        String textbeispiel = "2hallo2waslos2";
+        String ausschnitt = "2";
+        suche.search(textbeispiel, ausschnitt);
+
+      /*  Main dings = new Main();
+        String[] hi = new String[1];
+        dings.main(hi);*/
+
+        System.out.println("Hello World!1");
+        System.out.println((char)97);
     }
 }
